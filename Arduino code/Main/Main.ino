6d4aff10e3e5d4e20018bbbbec6014ed2f02c54d
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-03 18:00:13 -0500

    Open port
    Open serial port on Arduino

--- Arduino code/Main/Main.ino
+++ Arduino code/Main/Main.ino
@@ -1,5 +1,20 @@
+/* 
+   
+ Reference:
+
+Arduino:
+Serial.begin() = Sets the data rate in bits per second (baud) for serial data transmission.
+
+Connections:
+Arduino Nano
+0 = RX 1 = TX
+
+*/
+int baudRate = 9600;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(baudRate);
+  Serial.println("Serial port open");
 }
 
 void loop() {
