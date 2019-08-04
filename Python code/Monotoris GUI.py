8009e8c568721b7174bb63c4ac0295b2b49b052a
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-03 23:06:52 -0500

    Update Monotoris GUI.py
    Clean the initial file for this branch

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -1,12 +1,3 @@
 
 # Reference:
 
-# pyserial:
-# serial.Serial = The port is immediately opened on object creation, when a port is given.
-
-import serial
-
-arduinoPort = serial.Serial('COM6', 9600, timeout=1)
-arduinoData = arduinoPort.readlines()
-
-print (arduinoData)
