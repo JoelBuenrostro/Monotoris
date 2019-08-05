4463289e0e3e02c0bef9ebc3c25bddccfbbeeaca
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-05 10:37:11 -0500

    Update Main.ino
    Set buzzer pin

--- Arduino code/Main/Main.ino
+++ Arduino code/Main/Main.ino
@@ -2,18 +2,19 @@
   Reference:
 
   Arduino:
-  Serial.begin() = Sets the data rate in bits per second (baud) for serial data transmission.
+  pinMode()        = Configures the specified pin to behave either as an input or an output.
+  Serial.begin()   = Sets the data rate in bits per second (baud) for serial data transmission.
   Serial.println() = Prints data to the serial port followed by a carriage return and a newline
 
   Connections:
   Arduino Nano
-  0 = RX 1 = TX
+  0=RX 1=TX 2=Buzzer
 
 */
-
+int buzzer_pin = 2;
 
 void setup() {
-  
+  pinMode(buzzer_pin, OUTPUT);
 }
 
 void loop() {
