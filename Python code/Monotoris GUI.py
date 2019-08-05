e99a97091103653c1a6a093b3e28d0c89e2576fc
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-05 13:45:16 -0500

    Update Monotoris GUI.py
    Add baudrate label

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -9,16 +9,22 @@ from tkinter import ttk
 import serial
 
 window = tk.Tk()
+user_font = "Arial"
+font_size = 16
 arduinoPort = serial.Serial('COM4', 9600)
 
 window.title("Monotoris")
 window.geometry("600x600")
 window.minsize(width=600, height=600)
 
-label1 = tk.Label(window, text="Port name : ", font=("Arial", 16))
-label2 = tk.Label(window, text=arduinoPort.name, font=("Arial", 16))
+label1 = tk.Label(window, text="Port name : ", font=(user_font, font_size))
+label2 = tk.Label(window, text=arduinoPort.name, font=(user_font, font_size))
+label3 = tk.Label(window, text="Baudrate : ", font=(user_font, font_size))
+label4 = tk.Label(window, text=arduinoPort.baudrate, font=(user_font, font_size))
 
 label1.grid(column=0, row=0)
 label2.grid(column=1, row=0)
+label3.grid(column=2, row=0)
+label4.grid(column=3, row=0)
 
 window.mainloop()
