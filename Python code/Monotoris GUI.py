7674b7b6811694473fec3c0d267d75b5d1e74da8
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-05 14:50:09 -0500

    Update Monotoris GUI.py
    Update layout

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -14,17 +14,18 @@ font_size = 16
 arduinoPort = serial.Serial('COM4', 9600)
 
 window.title("Monotoris")
-window.geometry("600x600")
-window.minsize(width=600, height=600)
+window.geometry("300x300")
+window.minsize(width=150, height=150)
 
 label1 = tk.Label(window, text="Port name : ", font=(user_font, font_size))
 label2 = tk.Label(window, text=arduinoPort.name, font=(user_font, font_size))
 label3 = tk.Label(window, text="Baudrate : ", font=(user_font, font_size))
 label4 = tk.Label(window, text=arduinoPort.baudrate, font=(user_font, font_size))
+label5 = tk.Label(window, text="Temperature : ", font=(user_font, font_size))
 
 label1.grid(column=0, row=0)
 label2.grid(column=1, row=0)
-label3.grid(column=2, row=0)
-label4.grid(column=3, row=0)
+label3.grid(column=0, row=1)
+label4.grid(column=1, row=1)
 
 window.mainloop()
