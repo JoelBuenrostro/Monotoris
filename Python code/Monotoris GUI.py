f0be35a86714de4cdd226cd1a2701522708ce2c3
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-05 09:58:15 -0500

    Update Monotoris GUI.py
    Firts main window

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -1,6 +1,18 @@
 
 # Reference:
 
-# pyserial:
-# serial.Serial = The port is immediately opened on object creation, when a port is given.
+# tkinter:
+# 
 
+import tkinter as tk
+import easygui
+import pandas as pd
+import time
+
+window = tk.Tk()
+
+window.title("Monotoris")
+window.geometry("600x600")
+window.minsize(width=600, height=600)
+
+window.mainloop()
