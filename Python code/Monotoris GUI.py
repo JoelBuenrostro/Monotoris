83b0bc4ba0d0b69b553913bcb916c47aa754c7d8
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-05 13:24:43 -0500

    Update Monotoris GUI.py
    Add label for display the port name

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -5,14 +5,20 @@
 # 
 
 import tkinter as tk
-import easygui
-import pandas as pd
-import time
+from tkinter import ttk
+import serial
 
 window = tk.Tk()
+arduinoPort = serial.Serial('COM4', 9600)
 
 window.title("Monotoris")
 window.geometry("600x600")
 window.minsize(width=600, height=600)
+
+label1 = tk.Label(window, text="Port name : ", font=("Arial", 16))
+label2 = tk.Label(window, text=arduinoPort.name, font=("Arial", 16))
+
+label1.grid(column=0, row=0)
+label2.grid(column=1, row=0)
 
 window.mainloop()
