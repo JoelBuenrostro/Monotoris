b4c648e9ebeeee1b5703d02637644f37b7ab612c
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-06 18:00:47 -0500

    Update Monotoris GUI.py
    More  readable example

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -4,39 +4,26 @@
 # tkinter:
 # 
 
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 import serial
 
-arduinoPort = serial.Serial('COM4', 9600, timeout=5)
-arduinoData = arduinoPort.readline()
+#arduinoPort = serial.Serial('COM4', 9600, timeout=5)
+#arduinoData = arduinoPort.readline()
 
-window = tk.Tk()
-user_font = "Arial"
-font_size = 16
-window.title("Monotoris")
-window.geometry("600x300")
-window.minsize(width=300, height=150)
+#Windows
+root = Tk()
 
-def update_label():
-    int(arduinoData)
-    label6.config(text=str(arduinoData))
-    window.after(1000, update_label)
+#Widgets
+mainframe = ttk.Frame(root, borderwidth=5, relief="sunken", width=200, height=100, padding=(3, 3, 12, 12))
+portlabel = ttk.Label(mainframe, text="Port :")
+baudlabel = ttk.Label(mainframe, text="Baudrate :")
+templabel = ttk.Label(mainframe, text="Temperature :")
 
+#Layout
+mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
+portlabel.grid(column=1, row=1, sticky=(N, W), padx=5)
+baudlabel.grid(column=1, row=2, sticky=(E, W), padx=5)
+templabel.grid(column=1, row=3, sticky=(N, S), padx=5)
 
-
-label1 = tk.Label(window, text="Port name : ", font=(user_font, font_size))
-label2 = tk.Label(window, text=arduinoPort.name, font=(user_font, font_size))
-label3 = tk.Label(window, text="Baudrate : ", font=(user_font, font_size))
-label4 = tk.Label(window, text=arduinoPort.baudrate, font=(user_font, font_size))
-label5 = tk.Label(window, text="Temperature : ", font=(user_font, font_size))
-label6 = tk.Label(window, text=arduinoData, font=(user_font, font_size))
-
-label1.grid(column=0, row=0)
-label2.grid(column=1, row=0)
-label3.grid(column=0, row=1)
-label4.grid(column=1, row=1)
-label5.grid(column=0, row=2)
-label6.grid(column=2, row=2)
-
-window.mainloop()
+root.mainloop()
