57c23bac282be21b56cbf6ea4860358cd9816553
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-07 11:01:09 -0500

    Update Monotoris GUI.py
    Add padding to labels

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -8,10 +8,11 @@ from tkinter import *
 from tkinter import ttk
 import serial
 
-#arduinoPort = serial.Serial('COM4', 9600, timeout=5)
+#arduinoPort = serial.Serial(port="COM4", baudrate=9600, timeout=5)
 #arduinoData = arduinoPort.readline()
 #tempdata = IntVar()
 #tempdata.set(arduinoData)
+
 
 #Windows
 root = Tk()
@@ -21,20 +22,20 @@ mainframe = ttk.Frame(root, borderwidth=5, relief="sunken", width=200, height=10
 portlabel = ttk.Label(mainframe, text="Port :")
 baudlabel = ttk.Label(mainframe, text="Baudrate :")
 templabel = ttk.Label(mainframe, text="Temperature :")
-porttext = ttk.Label(mainframe, text="COM4")
-baudtext = ttk.Label(mainframe, text="9600")
+portentry = ttk.Label(mainframe, text="COM4")
+baudentry = ttk.Label(mainframe, text="9600")
 tempread = ttk.Label(mainframe, text="23") #textvariable=tempdata
 
 #Layout
 #Column 0
 mainframe.grid(column=0, row=0, sticky=(N, S, E, W))
 #Column 1
-portlabel.grid(column=1, row=1, sticky=(N, W), padx=10, pady=5)
-baudlabel.grid(column=1, row=2, sticky=(E, W), padx=10, pady=5)
-templabel.grid(column=1, row=3, sticky=(N, S), padx=10, pady=5)
+portlabel.grid(column=1, row=1, sticky=(N, W), padx=20, pady=5)
+baudlabel.grid(column=1, row=2, sticky=(E, W), padx=20, pady=5)
+templabel.grid(column=1, row=3, sticky=(N, S), padx=20, pady=5)
 #Column 2
-porttext.grid(column=2, row=1, sticky=(N), padx=10, pady=5)
-baudtext.grid(column=2, row=2, sticky=(E, W), padx=10, pady=5)
-tempread.grid(column=2, row=3, sticky=(N, S), padx=10, pady=5)
+portentry.grid(column=2, row=1, sticky=(N), padx=20, pady=5)
+baudentry.grid(column=2, row=2, sticky=(E, W), padx=20, pady=5)
+tempread.grid(column=2, row=3, sticky=(N, S), padx=20, pady=5)
 
 root.mainloop()
