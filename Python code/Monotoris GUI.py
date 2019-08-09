bcc9c696dd11f6ee56ea1a05dcde9d7439622a39
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-08-09 15:24:01 -0500

    Update Monotoris GUI.py
    Change to a class style gui design

--- Python code/Monotoris GUI.py
+++ Python code/Monotoris GUI.py
@@ -5,31 +5,16 @@
 # 
 
 import tkinter as tk
-from tkinter import ttk
-import serial
 
-window = tk.Tk()
-user_font = "Arial"
-font_size = 16
-arduinoPort = serial.Serial('COM4', 9600, timeout=5)
-arduinoData = arduinoPort.readline(100)
+class window(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-window.title("Monotoris")
-window.geometry("600x300")
-window.minsize(width=300, height=150)
+        self.title("Monotoris")
+        self.geometry("300x400")
+        self.labelport = tk.Label(self, text="Port :", padx=5, pady=5)
+        self.labelport.pack()
 
-label1 = tk.Label(window, text="Port name : ", font=(user_font, font_size))
-label2 = tk.Label(window, text=arduinoPort.name, font=(user_font, font_size))
-label3 = tk.Label(window, text="Baudrate : ", font=(user_font, font_size))
-label4 = tk.Label(window, text=arduinoPort.baudrate, font=(user_font, font_size))
-label5 = tk.Label(window, text="Temperature : ", font=(user_font, font_size))
-label6 = tk.Label(window, text=arduinoData, font=(user_font, font_size))
-
-label1.grid(column=0, row=0)
-label2.grid(column=1, row=0)
-label3.grid(column=0, row=1)
-label4.grid(column=1, row=1)
-label5.grid(column=0, row=2)
-label6.grid(column=2, row=2)
-
-window.mainloop()
+if __name__ == "__main":
+    root = window()
+    root.mainloop()
